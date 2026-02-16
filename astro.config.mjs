86cb9efac1bf4ea0fd3e2cb89404c79182edb3bb
Santiago Pinzón Vásquez <Santipinzonv@gmail.com>
2026-02-15 21:54:31 -0500

    Update site URL and add base path in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://spinzonv.github.io', 
-  // Asegúrate de que tenga el https:// y que NO termine en una barra diagonal /
+  site: 'https://chocax.github.io', 
+  base: '/spinzonv.github.io', 
 });
